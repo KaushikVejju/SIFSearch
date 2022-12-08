83772e4d1ae746e9dee6118dedd5e95711196422
Kaushik <kaush2003@gmail.com>
2022-12-08 00:29:30 -0500

    search features implemented

--- sifsearchdemo/static/scripts.js
+++ sifsearchdemo/static/scripts.js
@@ -1,9 +1,22 @@
 let search_entries = [];
+/*
+let mainDiv = document.querySelector('.welcomediv');
+let searchDiv = document.createElement('div');
+searchDiv.classList.add('search-list');
+*/
+
 window.addEventListener('DOMContentLoaded', (event) => {
   document.getElementById("upload_success").style.display = "none";
 });
 
+let mainDiv = document.querySelector('.welcomediv');
+let searchDiv = document.createElement('div');
+searchDiv.classList.add('search-list');
+mainDiv.appendChild(searchDiv);
+
+
 function uploadFunction() {
+
   let entry = {
     title: document.getElementById('title').value,
     description: document.getElementById('description').value
@@ -12,12 +25,15 @@ function uploadFunction() {
   document.forms[0].reset();
   console.log(JSON.stringify(entry)); /* debugging */
   console.log(search_entries); /* debugging, making sure that values have been added into the list */
+  addSearchEntry(entry.title, entry.description)
   uploadSuccess(entry.title, entry.description)
+
 }
 function uploadSuccess(title,description) {
+  document.querySelector(".search-list").style.display = "none";
   document.getElementById("upload_div").style.display = "none";
   document.getElementById("upload_success").style.display = "block";
-  document.getElementById("title_success").innerHTML = title
+  document.getElementById("title_success").innerHTML = title;
   document.getElementById("descr_success").innerHTML = description
 
 }
@@ -25,10 +41,55 @@ function uploadSuccess(title,description) {
 function goBack() {
   document.getElementById("upload_success").style.display = "none";
   document.getElementById("upload_div").style.display = "block";
+  document.querySelector(".search-list").style.display = "none";
+
 }
 
-/*
-Plan for tmrw, be able to search for a specific entry and create an html element dynamically
-4 hours this should take 
-*/
+
+function addSearchEntry(title, description) {
+
+  console.log("bro");
+  let searchEntryDetails = `
+        <div id = "search_entry" class = "search_item"> 
+            <br>
+            <p class = "title_item">${title}</p>
+            <p class = "description_item">${description}</p>
+        </div>
+  `;
+  searchDiv.insertAdjacentHTML('beforeend',searchEntryDetails);
+
+}
+
+/* searching for an entry dynamically */
+
+function search () {
+  document.getElementById("upload_div").style.display = "none";
+  document.getElementById("upload_success").style.display = "none";
+  document.querySelector(".search-list").style.display = "";
+  console.log("bro");
+  const searchInput = document.getElementById("search-item").value;
+  const item = document.querySelectorAll(".search_item")
+  const titleName = document.querySelectorAll(".title_item");
+  const descrName = document.querySelectorAll(".description_item");
+  for (var i = 0; i < titleName.length; i++) {
+    console.log(titleName[i].innerHTML)
+    if(titleName[i].innerHTML == searchInput || descrName[i].innerHTML == searchInput) {
+      console.log("you have reached")
+      item[i].style.display = "block";
+    }
+    else {
+      item[i].style.display = "none";
+    }
+  }
+}
+
+function clearsearch() {
+  document.getElementById('search-item').value='';
+  document.querySelector(".search-list").style.display = "none";
+  document.getElementById("upload_div").style.display = "";
+
+}
+
+
+
 
