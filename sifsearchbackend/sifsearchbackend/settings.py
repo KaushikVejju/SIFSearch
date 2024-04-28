23a35e220b6982d2d259ef400df39b967cb44069
Kaushik <kaush2003@gmail.com>
2024-04-27 22:47:02 -0400

    revised setting.py

--- sifsearchbackend/sifsearchbackend/settings.py
+++ sifsearchbackend/sifsearchbackend/settings.py
@@ -30,9 +30,6 @@ DEBUG = True
 if os.getcwd() == '/app':
     DEBUG = False
 
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -138,12 +135,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
 MEDIA_ROOT = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
