5663e6c19f99ec47695aecb485f831e9affa63dc
Kaushik <kaush2003@gmail.com>
2024-04-28 11:20:12 -0400

    added static files to settings.py

--- sifsearchbackend/sifsearchbackend/settings.py
+++ sifsearchbackend/sifsearchbackend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
     'sifsearchapp',
@@ -133,6 +134,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
